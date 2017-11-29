86ad982ec93ea29dbec52547e5ab8ecaf045d860
Baom <bellomouhoutassim@gmail.com>
2017-11-29 22:21:48 +0100

    Premier commit pour la creation de commentaire

---
+++ src/main/java/com/bootcamp/controllers/CommentaireController.java
@@ -0,0 +1,130 @@
+package com.bootcamp.controllers;
+
+import com.bootcamp.commons.exceptions.DatabaseException;
+import com.bootcamp.commons.ws.models.CommentaireUWs;
+import com.bootcamp.commons.ws.models.Error;
+import com.bootcamp.entities.Commentaire;
+import com.bootcamp.services.CommentaireService;
+import com.bootcamp.version.ApiVersions;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.lang.reflect.InvocationTargetException;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@RestController("CommentaireController")
+@RequestMapping("/commentaire")
+@Api(value = "Commentaire API", description = "Commentaire API")
+public class CommentaireController {
+
+    @Autowired
+    CommentaireService commentaireService;
+
+    @Autowired
+    HttpServletRequest request;
+
+    @RequestMapping(method = RequestMethod.POST, value = "/")
+    @ApiVersions({"1.0"})
+    @ApiOperation(value = "Create a new commentaire", notes = "Create a new commentaire")
+    public ResponseEntity<CommentaireUWs> create(@RequestBody @Valid CommentaireUWs commentaireUWs) {
+
+        HttpStatus httpStatus = null;
+
+        int id;
+        try {
+            id = commentaireService.create(commentaireUWs);
+            commentaireUWs.setId(id);
+            httpStatus = HttpStatus.OK;
+        } catch (SQLException ex) {
+            Logger.getLogger(CommentaireController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return new ResponseEntity<CommentaireUWs>(commentaireUWs, httpStatus);
+    }
+//
+//
+//    @RequestMapping(method = RequestMethod.PUT, value = "/")
+//    @ApiVersions({"1.0"})
+//    @ApiOperation(value = "Update a new commentaire", notes = "Update a new commentaire")
+//    public ResponseEntity<CommentaireWs> update(@RequestBody @Valid Commentaire commentaire) {
+//
+//        CommentaireWs commentaireWs = new CommentaireWs();
+//        HttpStatus httpStatus = null;
+//
+//        try {
+//            commentaireService.update(commentaire);
+//            commentaireWs.setData(commentaire);
+//            httpStatus = HttpStatus.OK;
+//        }catch (SQLException exception){
+//            String errorMessage = exception.getMessage()==null?exception.getMessage():exception.getCause().getMessage();
+//            Error error = new Error();
+//            error.setMessage(errorMessage);
+//            commentaireWs.setError(error);
+//            httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+//        }
+//
+//        return new ResponseEntity<CommentaireWs>(commentaireWs, httpStatus);
+//    }
+//
+//    @RequestMapping(method = RequestMethod.DELETE, value = "/{id}")
+//    @ApiVersions({"1.0"})
+//    @ApiOperation(value = "Delete a commentaire", notes = "Delete a commentaire")
+//    public ResponseEntity<CommentaireWs> delete(@PathVariable(name = "id") int id) {
+//
+//        CommentaireWs commentaireWs = new CommentaireWs();
+//        HttpStatus httpStatus = null;
+//
+//        try {
+//            Commentaire commentaire = commentaireService.delete(id);
+//            commentaireWs.setData(commentaire);
+//            httpStatus = HttpStatus.OK;
+//        }catch (SQLException exception){
+//            String errorMessage = exception.getMessage()==null?exception.getMessage():exception.getCause().getMessage();
+//            Error error = new Error();
+//            error.setMessage(errorMessage);
+//            commentaireWs.setError(error);
+//            httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+//        }
+//
+//        return new ResponseEntity<CommentaireWs>(commentaireWs, httpStatus);
+//    }
+
+//    @RequestMapping(method = RequestMethod.GET, value = "/{id}")
+//    @ApiVersions({"1.0"})
+//    @ApiOperation(value = "Read a commentaire", notes = "Read a commentaire")
+//    public ResponseEntity<Commentaire> read(@PathVariable(name = "id") int id) {
+//
+//        Commentaire commentaireWs = new CommentaireWs();
+//        HttpStatus httpStatus = null;
+//
+//        try {
+//            Commentaire commentaire = commentaireService.read(id);
+//            commentaireWs.setData(commentaire);
+//            httpStatus = HttpStatus.OK;
+//        }catch (SQLException exception){
+//            String errorMessage = exception.getMessage()==null?exception.getMessage():exception.getCause().getMessage();
+//            Error error = new Error();
+//            error.setMessage(errorMessage);
+//            commentaireWs.setError(error);
+//            httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+//        }
+//
+//        return new ResponseEntity<CommentaireWs>(commentaireWs, httpStatus);
+//    }
+    @RequestMapping(method = RequestMethod.GET, value = "/")
+    @ApiVersions({"1.0"})
+    @ApiOperation(value = "Read a commentaire", notes = "Read a commentaire")
+    public ResponseEntity<List<Commentaire>> read() throws InvocationTargetException, SQLException, DatabaseException, IllegalAccessException {
+        List<Commentaire> commentaires = commentaireService.read(request);
+        return new ResponseEntity<List<Commentaire>>(commentaires, HttpStatus.OK);
+    }
+
+}
---
+++ src/main/java/com/bootcamp/services/CommentaireService.java
@@ -0,0 +1,80 @@
+package com.bootcamp.services;
+
+import com.bootcamp.commons.constants.DatabaseConstants;
+import com.bootcamp.commons.exceptions.DatabaseException;
+import com.bootcamp.commons.models.Criteria;
+import com.bootcamp.commons.models.Criterias;
+import com.bootcamp.commons.ws.models.CommentaireUWs;
+import com.bootcamp.commons.ws.utils.RequestParser;
+import com.bootcamp.crud.CommentaireCRUD;
+import com.bootcamp.entities.Commentaire;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.InvocationTargetException;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ * Created by darextossa on 11/27/17.
+ */
+@Component
+public class CommentaireService implements DatabaseConstants {
+
+    public int create(CommentaireUWs commentaireUWs) throws SQLException {
+        Commentaire commentaire = new Commentaire();
+        commentaire.setId(commentaireUWs.getId());
+        commentaire.setContenu(commentaireUWs.getContenu());
+        commentaire.setEntityType(commentaireUWs.getEntityType());
+        commentaire.setPseudo(commentaireUWs.getPseudo());
+        commentaire.setDateCreation(commentaireUWs.getDateCreation());
+        commentaire.setDateMiseAJour(commentaireUWs.getDateMiseAJour());
+        
+        CommentaireCRUD.create(commentaire);
+        return commentaire.getId();
+    }
+
+    public void update(CommentaireUWs commentaireUWs) throws SQLException {
+        Commentaire commentaire = new Commentaire();
+        commentaire.setId(commentaireUWs.getId());
+        commentaire.setContenu(commentaireUWs.getContenu());
+        commentaire.setEntityType(commentaireUWs.getEntityType());
+        commentaire.setPseudo(commentaireUWs.getPseudo());
+        commentaire.setDateCreation(commentaireUWs.getDateCreation());
+        commentaire.setDateMiseAJour(commentaireUWs.getDateMiseAJour());
+        
+        CommentaireCRUD.update(commentaire);
+    }
+
+    public Commentaire delete(int id) throws SQLException {
+        Commentaire commentaire = read(id);
+        CommentaireCRUD.delete(commentaire);
+
+        return commentaire;
+    }
+
+    public Commentaire read(int id) throws SQLException {
+        Criterias criterias = new Criterias();
+        criterias.addCriteria(new Criteria("id", "=", id));
+        List<Commentaire> commentaires = CommentaireCRUD.read(criterias);
+
+        return commentaires.get(0);
+    }
+
+    public List<Commentaire> read(HttpServletRequest request) throws SQLException, IllegalAccessException, DatabaseException, InvocationTargetException {
+        Criterias criterias = RequestParser.getCriterias(request);
+        List<String> fields = RequestParser.getFields(request);
+        List<Commentaire> commentaires = null;
+        if (criterias == null && fields == null) {
+            commentaires = CommentaireCRUD.read();
+        } else if (criterias != null && fields == null) {
+            commentaires = CommentaireCRUD.read(criterias);
+        } else if (criterias == null && fields != null) {
+            commentaires = CommentaireCRUD.read(fields);
+        } else {
+            commentaires = CommentaireCRUD.read(criterias, fields);
+        }
+
+        return commentaires;
+    }
+}
